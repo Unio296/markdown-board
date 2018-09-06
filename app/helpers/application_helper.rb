c001eaed6a5501a8ae784d3af953cef04ba0ebcc
unio <unioblog@gmail.com>
2018-09-07 06:14:19 +0900

    add Static Pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 module ApplicationHelper
+  # ページごとの完全なタイトルを返します。
+  def full_title(page_title = '')
+    base_title = "Markdown Board"
+    if page_title.empty?                                        #page_titleが空白の場合
+      base_title
+    else                                                        #page_titleが空白でない場合
+      page_title + " | " + base_title
+    end
+  end
+
+  
   #マークダウン用ヘルパー
   require "redcarpet"
   require "coderay"
