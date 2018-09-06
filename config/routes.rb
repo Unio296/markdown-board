73ea43f8fc9855f5a5338092b469a164ddabe227
unio <unioblog@gmail.com>
2018-09-07 07:12:33 +0900

    create Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root "static_pages#home"
   get '/terms' ,to: "static_pages#terms" 
   get '/privacy', to: "static_pages#privacy"
   get '/contact', to: "static_pages#contact"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
