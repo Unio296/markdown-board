8d033f072cfcb4dbab820711a9005cdafff03ae8
unio <unioblog@gmail.com>
2018-09-19 19:43:36 +0900

    heroku env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Rails.application.credentials.TWITTER_API_KEY, Rails.application.credentials.TWITTER_API_SECRET
-  #provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
+  #provider :twitter, Rails.application.credentials.TWITTER_API_KEY, Rails.application.credentials.TWITTER_API_SECRET
+  provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
 end
