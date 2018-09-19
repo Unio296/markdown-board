a36eb758f981271b2daec7f0767ad9f801063add
unio <unioblog@gmail.com>
2018-09-19 20:23:59 +0900

    test2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :twitter, Rails.application.credentials.TWITTER_API_KEY, Rails.application.credentials.TWITTER_API_SECRET
   provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
-  
+
 end
