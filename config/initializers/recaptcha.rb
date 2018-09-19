3e41f46ca5da32bba8c84ad38b789a4e9d47abc9
unio <unioblog@gmail.com>
2018-09-20 00:51:17 +0900

    お問い合わせフォームレイアウト調整

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,10 +1,10 @@
 Recaptcha.configure do |config|
 # development
-  #config.site_key  = Rails.application.credentials.RECAPTCHA_SITE_KEY
-  #config.secret_key = Rails.application.credentials.RECAPTCHA_SECRET_KEY
+  config.site_key = Rails.application.credentials.RECAPTCHA_SITE_KEY
+  config.secret_key = Rails.application.credentials.RECAPTCHA_SECRET_KEY
 
 # production
-  config.site_key  = ENV['RECAPTCHA_SITE_KEY']
-  config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
+  #config.site_key  = ENV['RECAPTCHA_SITE_KEY']
+  #config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
 
 end
