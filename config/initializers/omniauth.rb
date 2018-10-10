686c4e4f83d1a97baa25f4ed4fcf160ba6fd065a
unio <unioblog@gmail.com>
2018-10-10 12:33:25 +0900

    omniauth.rb productionとdevelopment環境それぞれに対応

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Rails.application.credentials.TWITTER_API_KEY, Rails.application.credentials.TWITTER_API_SECRET
+  #provider :twitter, Rails.application.credentials.TWITTER_API_KEY, Rails.application.credentials.TWITTER_API_SECRET
   #provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
 
+  if Rails.env.production?
+    provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_SECRET_KEY']
+  else
+    provider :twitter, Rails.application.credentials.TWITTER_API_KEY, Rails.application.credentials.TWITTER_API_SECRET
+    
+  end
 end
