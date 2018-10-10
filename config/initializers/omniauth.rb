21b1dba53f961d71f49ad65e093546c9ba396536
unio <unioblog@gmail.com>
2018-10-10 12:36:29 +0900

    omniauth.rb環境変数名修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
 
   if Rails.env.production?
-    provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_SECRET_KEY']
+    provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
   else
     provider :twitter, Rails.application.credentials.TWITTER_API_KEY, Rails.application.credentials.TWITTER_API_SECRET
     
